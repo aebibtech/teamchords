1cd4804f809dcd4c68f6fb26d20b0aadfd3f56f0
aebibtech <aebibtech@gmail.com>
2025-02-09 01:29:49 +0800

    improve home page

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -40,7 +40,7 @@ const Signin = () => {
   };
 
   return (
-    <div>
+    <div className="bg-gray-700 w-screen h-screen flex">
       <form onSubmit={handleSignIn} className="max-w-md m-auto mt-24 p-12 border rounded bg-gray-100">
         <h1 className="text-2xl mb-12 font-bold text-center">Team Chords</h1>
         <h2 className="font-bold pb-2">Sign in</h2>

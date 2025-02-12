e373be11ae341e1b144ed93a1c7f9163ef3dc590
aebibtech <aebibtech@gmail.com>
2025-02-13 06:37:57 +0800

    change main font, address chord sheet display issues on small screens

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div>
       <div className="flex justify-between p-2 shadow-md text-white bg-gray-700">
-        <h1 className="text-3xl my-auto">
+        <h1 className="my-auto">
           <Guitar size={32} className="inline-block" /> Team Chords
         </h1>
         <div className="flex">
@@ -15,15 +15,9 @@ function App() {
           >
             Sign in
           </Link>
-          {/* <Link
-            className="px-4 py-3 mt-1 rounded my-auto hover:bg-gray-500"
-            to="/signup"
-          >
-            Sign up
-          </Link> */}
         </div>
       </div>
-      <div className="text-center text-2xl font-bold">Home Page Content</div>
+      <div className="text-center font-bold">Home Page Content</div>
     </div>
   );
 }
